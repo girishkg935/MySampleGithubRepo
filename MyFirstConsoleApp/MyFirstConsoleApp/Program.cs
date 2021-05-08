f017c35096028b9e24fbc3946a5356782a7909c1
girishkg95 <34678175+girishkg95@users.noreply.github.com>
2021-05-08 17:56:33 +0530

    Program.cs file is modified now

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -11,6 +11,8 @@ namespace MyFirstConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wold from Console App");
+
+            Console.WriteLine("Hi guys, this file is modified with new content");
         }
     }
 }
